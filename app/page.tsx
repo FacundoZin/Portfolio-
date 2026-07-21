1ce29775ad575478335b03a534a5aacf9536f802
facu <facundozin10@gmail.com>
2026-07-20 23:40:25 -0300

    add utn logo to section education

--- app/page.tsx
+++ app/page.tsx
@@ -362,17 +362,15 @@ export default function Home() {
 
                   <div className="lg:col-span-3 flex items-start lg:justify-end mt-2 lg:mt-0">
                     <svg
-                      className="w-5 h-5 text-muted-foreground/30 group-hover:text-muted-foreground/60 transition-colors duration-500"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
+                      viewBox="0 0 595.3 699.4"
+                      className="w-7 h-8 text-muted-foreground/30 group-hover:text-muted-foreground/60 transition-colors duration-500"
+                      fill="currentColor"
                       aria-hidden="true"
                     >
                       <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={1.5}
-                        d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 3.741-1.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5"
+                        clipRule="evenodd"
+                        d="m246.6 0h102v190.8c80.8-22.4 140.4-96.7 140.4-184.4h106.3c0 146.5-106.8 268.9-246.6 293.2v4.4h233.9v104.2h-214.4c130 31.8 227 149.5 227 289.1h-106.2c0-87.7-59.6-162-140.3-184.4v186.5h-102v-186.5c-80.7 22.4-140.3 96.7-140.3 184.4h-106.4c0-139.6 97-257.3 227-289.1h-214.2v-104.2h233.9v-4.4c-139.9-24.3-246.7-146.7-246.7-293.2h106.3c0 87.7 59.6 162 140.3 184.4z"
+                        fillRule="evenodd"
                       />
                     </svg>
                   </div>
